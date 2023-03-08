a365dfec27c5c5b073c91aca154e4d97db649361
arafat-rimon <rimonarafat2020@gmail.com>
2023-03-08 23:02:41 +0600

    first commit

---
+++ src/main/java/com/example/erp_managemet_system/ErpManagemetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.erp_managemet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpManagemetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpManagemetSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/erp_managemet_system/ErpManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erp_managemet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
